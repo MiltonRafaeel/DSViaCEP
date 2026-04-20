d2b140e208218c90f28a21d21dd869a898ba5a86
MiltonRafael <miltonrafael166@gmail.com>
2026-04-19 23:31:51 -0300

    Creating a modal controller and a page controller

---
+++ projeto-cep/js/controllers/page-controller.js
@@ -0,0 +1,13 @@
+import * as modalController from './modal-controller.js';
+
+export function init() {
+    const contactLink = document.querySelector(".contact-link");
+    contactLink.addEventListener('click', handleContactLinkClick);
+
+    console.log("dasdasdasdasdasdsa")
+}
+
+function handleContactLinkClick(event) {
+    event.preventDefault();
+    modalController.showModal();
+}
--- projeto-cep/js/dsviacep.js
+++ projeto-cep/js/dsviacep.js
@@ -1,5 +1,11 @@
 import * as formController from './controllers/form-controller.js';
 import * as listContorller from './controllers/list-controller.js';
+import * as modalController from './controllers/modal-controller.js';
+import * as pageController from './controllers/page-controller.js';
+
+
 
 formController.init();
 listContorller.init();
+modalController.init();
+pageController.init();
